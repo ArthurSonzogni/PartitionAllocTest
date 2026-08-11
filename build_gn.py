21b13b7141d6df63177708545acbfad753ae823c
ArthurSonzogni <sonzogniarthur@gmail.com>
2026-08-11 09:56:18 +0200

    build_gn: target gn explicitly to avoid cipd dependency in CI

--- build_gn.py
+++ build_gn.py
@@ -11,8 +11,8 @@ def build_gn():
     if os.path.exists(gn_dir):
         # Fetch the latest changes if the directory already exists.
         subprocess.run(["git", "-C", gn_dir, "pull"], check=True)
-        return
-    
-    subprocess.run(["git", "clone", gn_repo, gn_dir], check=True)
+    else:
+        subprocess.run(["git", "clone", gn_repo, gn_dir], check=True)
+
     subprocess.run(["python", "build/gen.py"], cwd=gn_dir, check=True)
-    subprocess.run(["ninja", "-C", "out"], cwd=gn_dir, check=True)
+    subprocess.run(["ninja", "-C", "out", "gn"], cwd=gn_dir, check=True)
